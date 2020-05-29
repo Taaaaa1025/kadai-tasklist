c18f26ddc976144e71ef7948b008a517275c814c
Taichi.Nakamura <scenarioaaart2510@gmail.com>
2020-05-29 07:43:35 +0000

    add fix

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2020_05_29_011348) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.string "status"
   end
 
